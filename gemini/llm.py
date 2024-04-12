f435f045cea4e73c6b1c57d5de53d0ca03bba854
vistokartika <38373457+vistokartika@users.noreply.github.com>
2024-04-12 22:09:36 +0700

    Added append history

--- gemini/llm.py
+++ gemini/llm.py
@@ -1,6 +1,7 @@
 import os
 import PIL.Image
 import google.generativeai as genai
+from google.generativeai.types import content_types
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -29,31 +30,35 @@ class GeminiLLM():
     def query_with_image(self, image, prompt=None):
         if prompt is None:
             prompt = self.vision_prompt
-
-        response = self.chat.send_message(prompt)
-        self.chat.history = self.chat.history[:-1]
-        temp_history = self.chat.history[-1]
-        self.chat.history = self.chat.history[:-1]
         
         img = PIL.Image.open(image)
 
-        described = self.vision_model.generate_content(["Describe what's on the image", img], stream=True)
+        described = self.vision_model.generate_content(["Describe what's on the image in detail", img], stream=True)
         described.resolve()
         additional_response = self.query("Given a description:\n" + described.text + "\nIs the description has anything related to what's have been discussed by us? If there's then answer with \"Yes\" as the first word, otherwise \"No\" as the first word" )
 
         response = self.vision_model.generate_content([prompt, img], stream=True)
         response.resolve()
 
-        self.chat.history.append(temp_history)
-        self.chat.history.append(response.candidates[0].content)
+        c = content_types.to_content({"role": "user", "parts": prompt})
+        self.chat.history.append(c)
+        c = content_types.to_content({"role": "model", "parts": response.text})
+        self.chat.history.append(c)
         self.pop_history()
 
         initial_response = response.text
         
         return {"initial_response": initial_response, "additional_response": additional_response}
+    
+    def append_history(self, user_query, model_response):
+        c = content_types.to_content({"role": "user", "parts": user_query})
+        self.chat.history.append(c)
+        c = content_types.to_content({"role": "model", "parts": model_response})
+        self.chat.history.append(c)
 
 if __name__ == "__main__":
     gemini = GeminiLLM()
+    gemini.append_history("Hello, How are you?", "Amazing, always happy to help you, how can I help you?")
     response = gemini.query("Which is one of the best place to visit in India during summer?")
     print(response)
     response = gemini.query("Tell me more about that place in 50 words")
@@ -76,4 +81,3 @@ if __name__ == "__main__":
     print(response)
     response = gemini.query("What's other games have similar genre?")
     print(response)
-    
