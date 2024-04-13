b24b496bd2825c6919cf2b23ca82a12017cccae2
luisfrentzen <luisfrentzen@gmail.com>
2024-04-13 16:42:42 +0700

    added pet and vision base prompt, prepend base prompt on pop

--- gemini/llm.py
+++ gemini/llm.py
@@ -3,21 +3,38 @@ import PIL.Image
 import google.generativeai as genai
 from google.generativeai.types import content_types
 from dotenv import load_dotenv
+from prompts import SYSTEM_PET, SYSTEM_VISION, MODEL_AFFIRM
+
 load_dotenv()
 
 class GeminiLLM():
-    def __init__(self, max_n_history=20, vision_prompt="You are a friendly assistant disguised as a personal pet, Given a screenshot of user's desktop, your task is to describe what's happening and give a friendly or funny comment and give the user insight about the information related to what's happening on the screen."):
+    def __init__(self, max_n_history=20):
         genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+
         self.chat_model = genai.GenerativeModel('gemini-pro')
-        self.chat = self.chat_model.start_chat(history=[])
-        self.max_n_history = max_n_history
-        genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
         self.vision_model = genai.GenerativeModel('gemini-pro-vision')
-        self.vision_prompt = vision_prompt
+
+        self.vision_prompt = SYSTEM_VISION
+        self.system_prompt = SYSTEM_PET
+        self.affirm = MODEL_AFFIRM
+
+        self.content_to_prepend = [
+            content_types.to_content({"role": "user", "parts": SYSTEM_PET}), 
+            content_types.to_content({"role": "model", "parts": MODEL_AFFIRM})
+        ]
+
+        self.chat = self.chat_model.start_chat(history=self.content_to_prepend)
+        self.max_n_history = max_n_history
+
+    def prepend_system_prompt(self, clean_history):
+        return [*self.content_to_prepend, *clean_history]
 
     def pop_history(self):
-        if len(self.chat.history) > self.max_n_history:
-            self.chat.history = self.chat.history[-1 * self.max_n_history:]
+        if len(self.chat.history[2:]) <= self.max_n_history:
+            return
+        
+        chats = self.chat.history[2:]
+        self.chat.history = self.prepend_system_prompt(chats[2:])
     
     def reset_history(self):
         self.chat.history = []
@@ -33,11 +50,11 @@ class GeminiLLM():
         
         img = PIL.Image.open(image)
 
-        described = self.vision_model.generate_content(["Describe what's on the image in detail", img], stream=True)
+        described = self.vision_model.generate_content(["Describe what's on the image in detail", img])
         described.resolve()
         additional_response = self.query("Given a description:\n" + described.text + "\nIs the description has anything related to what's have been discussed by us? If there's then answer with \"Yes\" as the first word, otherwise \"No\" as the first word" )
 
-        response = self.vision_model.generate_content([prompt, img], stream=True)
+        response = self.vision_model.generate_content([prompt, img])
         response.resolve()
 
         c = content_types.to_content({"role": "user", "parts": prompt})
