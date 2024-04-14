2a06687877e59ffb2bef9830cadff0cf597183c2
William002 <william.nfs002@is-gs.com>
2024-04-14 23:18:52 +0700

    feat: add backgroud task screenshot and send via socket

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
+import threading
+import time
 from flask import Flask, request
+from flask_socketio import SocketIO, emit
 from gemini.llm import GeminiLLM
 import pyautogui
 from win32api import GetMonitorInfo, EnumDisplayMonitors
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 gemini = GeminiLLM()
 
 @app.route("/conversation")
@@ -31,3 +35,19 @@ def reset_history():
 @app.route("/screen-info")
 def screen_info():
     pass
+
+def background_screenshot_task():
+    while True:
+        im = pyautogui.screenshot()
+        im.save(f"monitor.png")
+        response = gemini.query_with_image("monitor.png", "What about this?")
+        print(response)
+        socketio.emit('screenshot', {'data': response})
+        time.sleep(5)
+        
+if __name__ == '__main__':
+    background_thread = threading.Thread(target=background_screenshot_task)
+    background_thread.daemon = True
+    background_thread.start()
+
+    socketio.run(app, debug=True)
